7fcc66de9f9ae43b987af229cc7c4d0e0df650f1
Jocelyn Dunkley <jdunkley@brynmawr.edu>
2020-07-07 20:52:37 -0400

    make it better

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: gunicorn django-ecommerce2 --log-file -
---
+++ venv/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/Users/BVD/Desktop/django-ecommerce2/ecommerce/venv/bin/python3.7
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
